397b37a902886513834ec3f75463b0899afd8c41
gmoroz <12345hasek@gmail.com>
2023-01-18 13:36:35 +0300

    add Address model

--- network/models.py
+++ network/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Address(models.Model):
+    country = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    street = models.CharField(max_length=100)
+    house_number = models.PositiveSmallIntegerField()
+
+
