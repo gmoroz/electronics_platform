2cd4c3b59e0019a90088f53c8f1944424c5c3d18
gmoroz <12345hasek@gmail.com>
2023-01-20 14:26:20 +0300

    start creating plant CRUD

--- network/urls.py
+++ network/urls.py
@@ -1,5 +1,11 @@
+from rest_framework.routers import SimpleRouter
 from django.urls import path
+
 from network import views
 
+network_router = SimpleRouter()
+network_router.register("plants", views.PlantViewSet, basename="plants")
 
 urlpatterns = []
+
+urlpatterns += network_router.urls
--- network/views.py
+++ network/views.py
@@ -2,9 +2,13 @@ from rest_framework import pagination, viewsets
 from . import models as net_models, serializers
 
 
+class BasePagination(pagination.PageNumberPagination):
+    page_size = 4
+
+
 class PlantViewSet(viewsets.ModelViewSet):
     queryset = net_models.Plant
-    pagination_class = pagination
+    pagination_class = BasePagination
 
     def get_serializer_class(self):
         if self.action == "list":
